f13da0e375c1eec3afe6b43a84aab9bf406934f4
Aslanbek Astamirov <asmmmmir@gmail.com>
2023-12-11 22:05:50 +0500

    updated input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -6,22 +6,30 @@ import { add } from '../../store/todoSlice/todoSlice';
 
 const Input = () => {
   const [title, setTitle] = useState<string>('');
+  const [error, setError] = useState<boolean>(false)
   const dispatch = useAppDispatch();
 
   const handleAddTodo = (e: React.MouseEvent) => {
     e.preventDefault();
-    dispatch(add({ title }));
-    setTitle('');
+    if(title.length > 3) {
+      dispatch(add({ title }));
+      setTitle('');
+      setError(false)
+    }
+    else {
+      setError(true)
+    }
   };
 
   return (
     <form className='input'>
       <input
+      className={`${error ? 'error' : ''}`}
         type="text"
         maxLength={30}
         placeholder='Create a new todo...'
         value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value )}
       />
       <button type="submit" style={{ background: 'transparent', border: 'none' }} onClick={handleAddTodo}>
         <RiAddBoxFill size='2.7rem' color='#8f4fe7' />
